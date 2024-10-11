32609100f7a7fd31d8238fa8b00ace99a868113e
MarksGB <131261270+22DP3MBruv@users.noreply.github.com>
2024-10-11 17:00:20 +0000

    Pievienoju modal un pievienoju card elementus mājaslapai

--- hamburg.js
+++ hamburg.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector('.hamburger');
-    const menuIcon = document.querySelector('.menu-icon');
-    const navMenu = document.querySelector('.nav-menu');
+    const open = document.getElementById('open');
+    const modal = new bootstrap.Modal('#modalid');
     
-    
-    if (menuIcon && navMenu && hamburger) {
-        menuIcon.addEventListener('click', function(event) {
-            event.stopPropagation();
-            hamburger.classList.toggle('active');
-            navMenu.classList.toggle('active');
-            
-        });
-    };
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        // menu-toggle ir checkbox kurš attaisa hamburger menu
+        // ja ir atvērts tad aizver, ja ir aizvērts tad atver
+        document.getElementById('menu-toggle').checked = !document.getElementById('menu-toggle').checked;
+    });
 
+    open.addEventListener('click', () => {
+        modal.show();
+    });
 });
