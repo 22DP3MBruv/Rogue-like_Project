ed77829af531773f5e5b889d8660ae79065945af
Marks Gerhards Brūveris <marks.bruveris@gmail.com>
2025-06-06 18:32:26 +0300

    Removed a redundant part of code

--- backend/api/updateAccount.php
+++ backend/api/updateAccount.php
@@ -10,7 +10,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 require_once '../config/database.php';
-require_once '../config/functions.php';
 
 $data = json_decode(file_get_contents("php://input"), true);
 
